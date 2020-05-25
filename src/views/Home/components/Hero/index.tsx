a167dc5ad05aa09121b555688e227959c83cdf20
Brunon80 <bruno_rocha2008@hotmail.com>
2020-05-25 17:22:55 -0300

    ajustes

--- src/views/Home/components/Hero/index.tsx
+++ src/views/Home/components/Hero/index.tsx
@@ -5,7 +5,7 @@ import homeBg from '../../../../assets/images/home-bg.png'
 
 const Hero: React.FC = () => {
     return (
-        <section className="hero-body">
+        <section className="section">
             <div className="container">
                 <div className="column centered">
                     <h1 className="title">Desenvolvedor Front-end especialista em React e React Native</h1>
