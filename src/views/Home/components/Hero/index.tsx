1fe62e9373898de9b6c07ac2b13b009302e86ac2
Brunon80 <bruno_rocha2008@hotmail.com>
2020-05-25 15:56:04 -0300

    ajustes

---
+++ src/views/Home/components/Hero/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import homeBg from '../../../../assets/images/home-bg.png'
+
+// import { Container } from './styles';
+
+const Hero: React.FC = () => {
+    return (
+        <section className="hero-body">
+            <div className="container">
+                <div className="column centered">
+                    <h1 className="title">Desenvolvedor Front-end especialista em React e React Native</h1>
+                    <h2 className="subtitle">Eu crio e desenvoldo interfaces, além de amar o que faço.</h2>
+                </div>
+                <div className="column centered">
+                    <img className="bg" src={homeBg} alt="background-home" />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
