223de617fbfd6e97c405faf9c1a93f96baab3598
Brunon80 <bruno_rocha2008@hotmail.com>
2020-05-27 09:10:41 -0300

    terminada area de contato

--- src/views/Contact/index.tsx
+++ src/views/Contact/index.tsx
@@ -1,5 +1,32 @@
 import React from 'react'
+import Container from '../../components/Container'
+import './styles.scss'
 
 export const Contact: React.FC = () => {
-    return <h2>Contact</h2>
+    return (
+        <Container sessionClassName="contact">
+            <div className="column centered">
+                <div className="wrapper is-three-quarters">
+                    <h2 className="title">Estou muito empolgado para saber como é seu projeto. Pronto para começar?</h2>
+                </div>
+                <form className="form" name="bruno-contact" method="POST" data-netlify="true" hidden>
+                    <div className="container-form">
+                        <div className="input-group">
+                            <label htmlFor="name">Name:</label>
+                            <input type="text" id="name" name="name" />
+                        </div>
+                        <div className="input-group">
+                            <label htmlFor="email">Email:</label>
+                            <input type="email" id="email" name="email" />
+                        </div>
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="details">Detalhes:</label>
+                        <textarea className="details" name="details" id="details" cols={30} rows={10}></textarea>
+                    </div>
+                    <button type="submit">Enviar</button>
+                </form>
+            </div>
+        </Container>
+    )
 }
